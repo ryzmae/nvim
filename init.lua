b56c7be4d91531458bf5c154d768038da9003aad
ryzm <114670723+ryzmae@users.noreply.github.com>
2025-08-14 15:00:35 +0200

    add theme

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.o.cmdheight = 0
 -- use a single global statusline
 vim.o.laststatus = 3
 
--- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("ryzm.find_replace").setup()
+
+vim.cmd.colorscheme('vesper')
