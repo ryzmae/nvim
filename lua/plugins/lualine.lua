fb69cee73adac934e5319eeaecccc8c966490490
ryzm <114670723+ryzmae@users.noreply.github.com>
2025-08-17 15:22:54 +0200

    fix lua line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,16 @@
 return {
   "nvim-lualine/lualine.nvim",
   event = "VeryLazy",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   opts = {
     options = {
       component_separators = "",
-      section_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
     },
     sections = {
       lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+      lualine_c = {},
       lualine_y = {
-        { "progress", separator = " ", padding = { left = 1, right = 0 } },
         { "location", padding = { left = 0, right = 1 } },
       },
       lualine_z = {
