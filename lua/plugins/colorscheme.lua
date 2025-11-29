85c2427cdaa57c5aecb96fa9ea380be019645b64
ryzm <114670723+ryzmae@users.noreply.github.com>
2025-11-29 13:26:19 +0100

    commit

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,25 @@
 return {
-  {
-    "datsfilipe/vesper.nvim",
-    lazy = false,
-    -- @type string
-    opts = {
-      transparent = true, -- Enable transparent background
-      italics = {
-        comments = true, -- Boolean: Italicizes comments
-        keywords = false, -- Boolean: Italicizes keywords
-        functions = true, -- Boolean: Italicizes functions
-        strings = false, -- Boolean: Italicizes strings
-        variables = false, -- Boolean: Italicizes variables
-      },
+  "datsfilipe/vesper.nvim",
+  lazy = false,
+  -- @type string
+  opts = {
+    transparent = true, -- Enable transparent background
+    italics = {
+      comments = true, -- Boolean: Italicizes comments
+      keywords = false, -- Boolean: Italicizes keywords
+      functions = true, -- Boolean: Italicizes functions
+      strings = false, -- Boolean: Italicizes strings
+      variables = false, -- Boolean: Italicizes variables
     },
   },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 }
+
+-- return {
+--   "vague-theme/vague.nvim",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other plugins
+--   config = function()
+--     require("vague").setup({})
+--     vim.cmd("colorscheme vague")
+--   end,
+-- }
---
+++ lua/plugins/wakatime.lua
@@ -0,0 +1,8 @@
+return {
+  "wakatime/vim-wakatime",
+  lazy = false, -- Load immediately since it's an essential tracking plugin
+  config = function()
+    -- You can add any specific WakaTime settings here if needed,
+    -- but for basic setup, it often works out of the box.
+  end,
+}
